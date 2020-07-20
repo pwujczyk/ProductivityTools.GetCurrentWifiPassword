88fc81cf1496abc76bedbd6abd4db24a66a35f26
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-07-20 18:19:01 +0200

    method body

--- ProductivityTools.PSGetCurrentWifiPassword/ProductivityTools.PSGetCurrentWifiPassword.psm1
+++ ProductivityTools.PSGetCurrentWifiPassword/ProductivityTools.PSGetCurrentWifiPassword.psm1
@@ -1,3 +1,19 @@
 function Get-CurrentWifiPassword {
+
+	[cmdletbinding()]
+	param()
+
+	Write-Verbose "Hello from Get-CurrentWifiPassword"
 	$wifiNetwork=Get-NetConnectionProfile -InterfaceAlias wi-fi
+	$wifiName=$wifiNetwork.Name
+	Write-Verbose "Current wifi name: $wifiName"
+	
+	$currentWifi=Get-WifiPassword -NetworkName $wifiName
+	$password=$currentWifi.Password
+	
+	Write-Verbose "Password: $password"
+	Write-Verbose "Bye bye from Get-CurrentWifiPassword"
+	return $password
 }
+
+Export-ModuleMember Get-CurrentWifiPassword
