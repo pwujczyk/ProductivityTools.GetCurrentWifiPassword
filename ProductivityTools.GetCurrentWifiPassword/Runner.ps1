9cca4b49f9b3045ac313836852437ce02ad35968
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-11-16 22:30:11 +0100

    rename

---
+++ ProductivityTools.GetCurrentWifiPassword/Runner.ps1
@@ -0,0 +1,5 @@
+clear
+cd $PSScriptRoot
+Import-Module .\ProductivityTools.GetCurrentWifiPassword.psm1 -Force
+
+Get-CurrentWifiPassword -Verbose
--- ProductivityTools.PSGetCurrentWifiPassword/Runner.ps1
+++
@@ -1,5 +0,0 @@
-clear
-cd $PSScriptRoot
-Import-Module .\ProductivityTools.PSGetCurrentWifiPassword.psm1 -Force
-
-Get-CurrentWifiPassword -Verbose
